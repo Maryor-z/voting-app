a02e93c03b0a9fbdc8f2e9f9dcc7d364f5af63db
Maryor-z <ogundipemayowa90@icloud.com>
2024-10-02 20:34:06 +0100

    new-commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// var firstName = prompt('What is you First name?');
+// var lastName = prompt('What is you last name?');
+// var age = prompt('How old are you?');
+// if (Number(age) < 18) {
+//     alert('You are not eligible to vote');
+// }
+// else {
+//     alert('You are eligible to vote, you are old enough');
+// }
+// console.log(firstName, typeof age);
---
+++ app.ts
@@ -0,0 +1,12 @@
+
+// const firstName = prompt('What is you First name?');
+// const lastName = prompt('What is you last name?');
+// const age = prompt('How old are you?');
+
+// if (Number(age) < 18) {
+//     alert('You are not eligible to vote');
+// } else {
+//     alert('You are eligible to vote, you are old enough');
+// }
+
+// console.log(firstName, typeof age);
